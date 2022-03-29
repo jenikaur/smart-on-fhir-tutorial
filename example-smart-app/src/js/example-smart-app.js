365e5fe5113257f38ad3397d48cb406a10092480
jenikaur <jeni.2013.27@gmail.com>
2022-03-29 10:25:32 +0530

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -32,6 +32,7 @@
           var lname = '';
 
           if (typeof patient.name[0] !== 'undefined') {
+            console.log("check:",patient.name[0]);
             fname = patient.name[0].given && patient.name[0].given.join(' ');
             lname = patient.name[0].family && patient.name[0].family.join(' ');
           }
